ab35f2b55e09129d93901a22f9b241d1e9a64278
zituhossain <zituhossain@gmail.com>
2022-06-15 14:52:33 +0600

    final output

--- script.js
+++ script.js
@@ -12,61 +12,109 @@ let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
 let gameOver = false;
+const players = ['p1', 'p2'];
+let turnPlayer = players[Math.floor(Math.random() * players.length)];
 
 // update playingtoScore into DOM
 playingToElm.textContent = winningScore;
+
+// disable playerBtn based on turn
+turnPlayer === 'p1' ?
+    p2BtnElm.setAttribute('disabled', 'disabled') :
+    p1BtnElm.setAttribute('disabled', 'disabled');
 
 function validationInput(score) {
     if (score < 1) {
         alert('please provide a value more than 0')
     }
 }
+function handleWinningState() {
+    if(p1Score === winningScore || p2Score === winningScore) {
+        gameOver = true;
+        // disabled player 1 or player 2 btn
+        p1BtnElm.setAttribute('disabled', 'disabled');
+        p2BtnElm.setAttribute('disabled', 'disabled');
+        alert(+ winningScore+ ' is the winner');
+    }
+}
+function resetValues() {
+    p1Score = 0;
+    p2Score = 0;
+    winningScore = 5;
+    gameOver = false;
+    turnPlayer = 'p1';
+    p1BtnElm.removeAttribute('disabled');
+    p2BtnElm.removeAttribute('disabled');
+    p1scoreElm.textContent = p1Score;
+    p2scoreElm.textContent = p2Score;
+    playingToElm.textContent = winningScore;
+}
+
 // input and update playing to score
 formElm.addEventListener('submit', (evt) => {
     evt.preventDefault();
+    // reset previous state
+    resetValues();
     // getting the input
     const inputScore = inputScoreElm.value;
     // validation check
     validationInput(inputScore);
+    // saving it into data layer
+    winningScore = +inputScore;
     // showing value on dom
-    playingToElm.textContent = inputScore;
+    playingToElm.textContent = winningScore;
     // reset the input
     inputScoreElm.value = '';
 });
 
 p1BtnElm.addEventListener('click', (evt) => {
     // increase the content
-    if (!gameOver && p1Score < winningScore) {
+    if (turnPlayer === 'p1' && !gameOver && p1Score < winningScore) {
         p1Score++;
         // update DOM
         p1scoreElm.textContent = p1Score;
-    }
-    if (p1Score === winningScore) {
-        gameOver = true;
-        // disabled player 1 or player 2 btn
+        turnPlayer = 'p2';
+        // disable p1
         p1BtnElm.setAttribute('disabled', 'disabled');
-        p2BtnElm.setAttribute('disabled', 'disabled');
-        // show messages
-        alert('Player-1 is winner');
+        // enable p2
+        p2BtnElm.removeAttribute('disabled');
     }
+
+    handleWinningState();
+
+    // if (p1Score === winningScore) {
+    //     gameOver = true;
+    //     // disabled player 1 or player 2 btn
+    //     p1BtnElm.setAttribute('disabled', 'disabled');
+    //     p2BtnElm.setAttribute('disabled', 'disabled');
+    //     // show messages
+    //     alert('Player-1 is winner');
+    // }
 
 });
 
 p2BtnElm.addEventListener('click', (evt) => {
     // increase the content
-    if (!gameOver && p2Score < winningScore) {
+    if (turnPlayer === 'p2' && !gameOver && p2Score < winningScore) {
         p2Score++;
         // update DOM
         p2scoreElm.textContent = p2Score;
-    }
-    if (p2Score === winningScore) {
-        gameOver = true;
-        // disabled player 1 or player 2 btn
-        p1BtnElm.setAttribute('disabled', 'disabled');
+        turnPlayer = 'p1';
+        // disable p2
         p2BtnElm.setAttribute('disabled', 'disabled');
-        // show messages
-        alert('Player-2 is winner');
+        // enable p1
+        p1BtnElm.removeAttribute('disabled');
     }
-
-    
+    handleWinningState();
+    // if (p2Score === winningScore) {
+    //     gameOver = true;
+    //     // disabled player 1 or player 2 btn
+    //     p1BtnElm.setAttribute('disabled', 'disabled');
+    //     p2BtnElm.setAttribute('disabled', 'disabled');
+    //     // show messages
+    //     alert('Player-2 is winner');
+    // }
 });
+
+// Reset the button
+resetBtnElm.addEventListener('click', resetValues);
